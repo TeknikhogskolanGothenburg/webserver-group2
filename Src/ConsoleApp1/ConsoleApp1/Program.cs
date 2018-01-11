270e83706359ca57d4cb75bbb579aaa5a295c821
Adrianaxj <35222944+Adrianaxj@users.noreply.github.com>
2018-01-11 12:55:54 +0100

    Respones.ContentType

--- Src/ConsoleApp1/ConsoleApp1/Program.cs
+++ Src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -39,13 +39,32 @@ namespace ConsoleApp1
 
                 // Obtain a response object.
                 HttpListenerResponse response = context.Response;
-                response.ContentType = "text/html";
+                switch (request.RawUrl.Split(new[] { '.' }).Last())
+                {
+                    case "jpg": response.ContentType = "image/jpg"; break;
+                    case "htm": response.ContentType = "htm"; break;
+                    case "html": response.ContentType = "text/html"; break;
+                    case "css": response.ContentType = "css"; break;
+                    case "js": response.ContentType = "js"; break;
+                    case "gif": response.ContentType = "gif"; break;
+                    case "pdf": response.ContentType = "pdf"; break;
+
+                }
+                if (request.RawUrl.EndsWith(".html") || request.RawUrl.EndsWith(".htm"))
+                {
+                    response.ContentType = "text/html";
+                }
+                if (request.RawUrl.EndsWith(".jg") || request.RawUrl.EndsWith(".htm"))
+                {
+                    
+                }
                 response.ContentEncoding = System.Text.Encoding.UTF8;
                 // Construct a response.
                 //string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
                 //byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
                 byte[] buffer = File.ReadAllBytes("Content/" + request.RawUrl);
                 Directory.GetFiles("Content");
+                
                 // Get a response stream and write the response to it.
                 response.ContentLength64 = buffer.Length;
                 Stream output = response.OutputStream;
